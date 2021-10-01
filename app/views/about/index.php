4deba42af75c49c9695e77d58927a434fa3034d4
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-10-01 19:15:54 +0700

    menambahkan assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profil.jpg" width="200" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, saya berasal dari <?= $data['asal']; ?>. Saya berumur <?= $data['umur']; ?>.</p>
+</div>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,3 +1,6 @@
+<script src="<?= BASEURL; ?>/js/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="<?php BASEURL; ?>/js/bootstrap.js"></script>
 </body>
 
 </html>
