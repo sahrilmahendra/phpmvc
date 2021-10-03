0a1879ab75db5d4db49242eefa45f4becefd29c5
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-10-03 07:54:34 +0700

    menambahkan insert data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,11 @@
-<div class="container mt-5">
+<div class="container mt-3">
     <div class="row">
         <div class="col-6">
-            <h3>Daftar Mahasiswa</h3>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+            <h3 class="mt-3">Daftar Mahasiswa</h3>
 
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
@@ -11,6 +15,57 @@
                     </li>
                 <?php endforeach; ?>
             </ul>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+
+                    <div class="form-group">
+                        <label for="nama">Nama :</label>
+                        <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan Nama">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nim">NIM :</label>
+                        <input type="number" class="form-control" id="nim" name="nim" placeholder="Masukkan NIM">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email :</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Masukkan Email">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="jurusan">Jurusan :</label>
+                        <select class="form-control" name="jurusan" id="jurusan">
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Mesin">Teknik Mesin</option>
+                            <option value="Teknik Sipil">Teknik Sipil</option>
+                            <option value="Teknik Elektro">Teknik Elektro</option>
+                        </select>
+                    </div>
+
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+            </div>
+            </form>
         </div>
     </div>
 </div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $data['judul'] ?></title>
-    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.min.css">
 </head>
 
 <body>
